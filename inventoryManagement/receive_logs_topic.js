1411967c383ea1b919b7f1b21e6eecc01da57d47
Michael Pabon <michael@michaels-air.lan>
2020-03-13 22:08:12 -0400

    added update inventory functionality

--- inventoryManagement/receive_logs_topic.js
+++ inventoryManagement/receive_logs_topic.js
@@ -2,14 +2,12 @@
 
 var amqp = require('amqplib/callback_api');
 const axios = require('axios');
-
 var args = process.argv.slice(2);
 
 if (args.length == 0) {
   console.log("Usage: receive_logs_topic.js <facility>.<severity>");
   process.exit(1);
 }
-
 
 amqp.connect('amqp://rabbitmq:5672', function (error0, connection) {
   if (error0) {
@@ -20,11 +18,9 @@ amqp.connect('amqp://rabbitmq:5672', function (error0, connection) {
       throw error1;
     }
     var exchange = 'orders';
-
     channel.assertExchange(exchange, 'topic', {
       durable: false
     });
-
     channel.assertQueue('', {
       exclusive: true
     }, function (error2, q) {
@@ -32,34 +28,16 @@ amqp.connect('amqp://rabbitmq:5672', function (error0, connection) {
         throw error2;
       }
       console.log(' [*] Waiting for logs. To exit press CTRL+C');
-
       args.forEach(function (key) {
         channel.bindQueue(q.queue, exchange, key);
       });
-      //Consume Order Created Message &
+      //Consume Order Created Message
       channel.consume(q.queue, function (msg) {
         console.log(" [x] %s:'%s'", msg.fields.routingKey, msg.content.toString());
-        var message = JSON.parse(msg.content);
-        var bookId = message.bookId;
-        var orderQuantity = message.orderQuantity;
-        checkStock(bookId);
-
-        //Get quantity of 'bookId` in stock
-
-        
-        //Publish message to webhook server
-        /*
-        const options = {
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        };
-        axios.post('http://webeventd:9292/private/publish-message', msg.content, options).then((response) => {
-          console.log(response);
-        }, (error) => {
-          console.log(error);
-        });
-        */
+        let message = JSON.parse(msg.content);
+        let bookId = message.bookId;
+        let orderQuantity = message.orderQuantity;
+        updateInventory(bookId, orderQuantity);
       }, {
         noAck: true
       });
@@ -67,13 +45,27 @@ amqp.connect('amqp://rabbitmq:5672', function (error0, connection) {
   });
 });
 
-function checkStock(bookId){
-  axios.get('http://inventory-api:4567/books/' + bookId)
+async function updateInventory(bookId, orderQuantity){
+  //Get quantity on hand
+  let promise = axios.get('http://inventory-api:4567/books/' + bookId)
   .then((response) => {
-    console.log(response.data);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
+    return response.data;
+  });
+  let result = await promise;
+  console.log(result);
+  let quantityOnHand = result.quantityOnHand;
+  let newQuantityOnHand = quantityOnHand - orderQuantity;
+  //Update inventory quantity
+  axios({
+    method: 'put',
+    url: 'http://inventory-api:4567/books/' + bookId,
+    data: {
+      newQuantityOnHand: newQuantityOnHand
+    }
+  })
+  .then((response) => {
+    console.log(response);
+  }, (error) => {
+    console.log(error);
   });
 }
